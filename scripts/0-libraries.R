33548c5273c16131db5c17706ea0724d2b322fcd
C. Sean Burns <sean.csb@gmail.com>
2021-05-20 17:20:41 -0400

    added comments that describe use of package

--- scripts/0-libraries.R
+++ scripts/0-libraries.R
@@ -1,7 +1,7 @@
-library("car")
+library("car") # for Anova function
 library("dplyr")
 library("forestmodel") # for forest_model plots
 library("ggpubr") # for ggline
 library("haven") # import SPSS files
-library("psych")
-library("sjstats")
+library("psych") # describeBy function
+library("sjstats") # for anova_stats

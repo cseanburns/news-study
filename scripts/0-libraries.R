fe078a69f90d5496b1974cc3759f0239aa959ce8
C. Sean Burns <sean.csb@gmail.com>
2021-05-21 13:43:17 -0400

    removed gridExtra package; not using

--- scripts/0-libraries.R
+++ scripts/0-libraries.R
@@ -3,7 +3,6 @@ library("dabestr") # for Anova plots
 library("dplyr")
 library("forestmodel") # for forest_model plots
 library("ggpubr") # for ggline
-library("gridExtra") # grid.arrange function
 library("haven") # import SPSS files
 library("psych") # describeBy function
 library("sjstats") # for anova_stats

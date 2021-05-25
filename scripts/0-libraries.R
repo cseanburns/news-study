2d7c0b3dc54feaaf98297759bf7d58f70c83a0da
C. Sean Burns <sean.csb@gmail.com>
2021-05-24 20:51:18 -0400

    added gridExtra plyr and pwr packages

--- scripts/0-libraries.R
+++ scripts/0-libraries.R
@@ -3,7 +3,10 @@ library("dabestr") # for Anova plots
 library("dplyr")
 library("forestmodel") # for forest_model plots
 library("ggpubr") # for ggline
+library("gridExtra") # for grid.arrange
 library("haven") # import SPSS files
+library("plyr")
 library("psych") # describeBy function
+library("pwr")
 library("sjstats") # for anova_stats
 library("stargazer") # for exporting models as a table

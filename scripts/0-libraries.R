34031b4dd4c10156263845e1549ca1e506f7be32
C. Sean Burns <sean.csb@gmail.com>
2021-05-27 13:48:47 -0400

    removed pwr (not used); reorganized

--- scripts/0-libraries.R
+++ scripts/0-libraries.R
@@ -1,12 +1,18 @@
+## Anova and statistic helpers
 library("car") # for Anova function
-library("dabestr") # for Anova plots
+library("psych") # describeBy function
+library("sjstats") # for anova_stats
+
+## Data helpers
 library("dplyr")
+library("plyr")
+
+## Plotting helpers
+library("dabestr") # for Anova plots
 library("forestmodel") # for forest_model plots
 library("ggpubr") # for ggline
 library("gridExtra") # for grid.arrange
+
+# Import and exporting data
 library("haven") # import SPSS files
-library("plyr")
-library("psych") # describeBy function
-library("pwr")
-library("sjstats") # for anova_stats
 library("stargazer") # for exporting models as a table
